29b3b18cec6803ffe0d863cecd4d2c00b07efb26
Muslim <max2dgame12@gmail.com>
2025-09-24 15:51:51 +0300

    fix some probloms

--- src/app/addNote/page.js
+++ src/app/addNote/page.js
@@ -19,7 +19,7 @@ const AddNote = () => {
                     <h1 className="text-3xl sm:text-4xl md:text-5xl">Add Note</h1>
 
                     <form className="flex flex-col !gap-10 !w-full items-center justify-center" onSubmit={handleSubmit}>
-                        <input value={data.title} onChange={(e) => setData({ ...data, title: e.target.value })} placeholder="Title" type="text" className="w-full bg-secdanry !px-5 !py-4 rounded-2xl text-xl border-none outline-none hover:scale-103 transform transition-all duration-300" />
+                        <input value={data.title} required onChange={(e) => setData({ ...data, title: e.target.value })} placeholder="Title" type="text" className="w-full bg-secdanry !px-5 !py-4 rounded-2xl text-xl border-none outline-none hover:scale-103 transform transition-all duration-300" />
                         <textarea value={data.des} onChange={(e) => setData({ ...data, des: e.target.value })} placeholder="description" type="text" className="w-full bg-secdanry !px-5 !py-4 rounded-2xl text-xl border-none outline-none hover:scale-103 transform transition-all duration-300"></textarea>
                         <button type="submit" className="!px-4 !py-5 bg-secdanry rounded-2xl md:w-2/3 w-3/4 font-bold transform hover:translate-y-1 transition-all duration-300 cursor-pointer lg:w-1/2">Add</button>
                     </form>
